8b31b1854e37654b1e819c43b8244cb9ca15dd6c
jiweiqing <jiweiqing7@hotmail.com>
2024-01-07 22:36:09 +0800

    add codes.

---
+++ src/IdentityService.Infrastructure/IdentityDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityService.Infrastructure
+{
+    public class IdentityDbContext:DbContext
+    {
+        public IdentityDbContext(DbContextOptions<IdentityDbContext> options):base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+            //modelBuilder.EnableSoftDeletionGlobalFilter();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/IdentityService/Program.cs
@@ -0,0 +1,135 @@
+using IdentityService.Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Serilog;
+using System.Text;
+using System.Text.Json.Serialization;
+using MediatR;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// log
+builder.Host.UseSerilog((hostingContext, loggerConfiguration) =>
+{
+    loggerConfiguration
+    .ReadFrom.Configuration(hostingContext.Configuration)
+    .Enrich.FromLogContext();
+});
+
+// dbcontext
+builder.Services.AddDbContext<IdentityDbContext>(options =>
+{
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), new MySqlServerVersion(builder.Configuration["Mysql:Version"]));
+});
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles); ;
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddHealthChecks();
+
+// Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllCrosDomainsPolicy", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+// swagger
+builder.Services.AddSwaggerGen(options =>
+{
+    // TODO: API 版本控制
+    options.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Title = "后台管理Api文档",
+        Version = "v1",
+        Description = "后台管理Api文档v1"
+    });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "JSON Web Token to access resources. Example: Bearer {token}",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+       {
+         new OpenApiSecurityScheme
+         {
+            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+         },
+         new [] { string.Empty }
+       }
+    });
+});
+
+// authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        // https 默认为true
+        //options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecurityKey"]!)),
+            ClockSkew = TimeSpan.FromMinutes(1) // 偏差
+        };
+
+        // options.Events 事件处理
+    });
+
+// memory cache
+builder.Services.AddMemoryCache();
+
+// todo: redis
+
+// 给接口添加返回值描述
+// builder.Services.TryAddEnumerable(ServiceDescriptor.Transient<IApplicationModelProvider, ProduceResponseTypeModelProvider>());
+
+// auto mapper
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// 配置模型校验返回错误格式
+//builder.Services.ConfigureApiBehaviorOptions();
+
+// TODO:
+// builder.Services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "IdentityService.WebAPI v1"));
+}
+
+app.UseCors("AllCrosDomainsPolicy");
+
+app.UseForwardedHeaders();
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
